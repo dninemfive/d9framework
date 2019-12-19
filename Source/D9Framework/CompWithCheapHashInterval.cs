060de5e7ee243638dce7d50efcb0193566d13be5
dninemfive <11084010+dninemfive@users.noreply.github.com>
2019-12-19 10:13:27 -0800

    add CompWithCheapHashInterval

---
+++ Source/D9Framework/CompWithCheapHashInterval.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+
+namespace D9Framework
+{
+    abstract class CompWithCheapHashInterval : ThingComp
+    {
+        private int? hashOffset = null;
+        public int TickInterval;
+        public bool IsCheapIntervalTick => (int)(Find.TickManager.TicksGame + hashOffset) % TickInterval == 0;
+
+        public override void PostSpawnSetup(bool respawningAfterLoad)
+        {
+            base.PostSpawnSetup(respawningAfterLoad);
+            hashOffset = parent.thingIDNumber.HashOffset();
+        }
+
+    }
+}
