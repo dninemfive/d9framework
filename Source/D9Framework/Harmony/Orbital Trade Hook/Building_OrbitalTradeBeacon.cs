ed929c5ae57f2051a9caebc577f148c36c956cde
dninemfive <11084010+dninemfive@users.noreply.github.com>
2020-01-20 22:30:47 -0800

    implement lbmaian's suggestion. untested.

--- Source/D9Framework/Harmony/Orbital Trade Hook/Building_OrbitalTradeBeacon.cs
+++ Source/D9Framework/Harmony/Orbital Trade Hook/Building_OrbitalTradeBeacon.cs
@@ -4,14 +4,16 @@ using System.Linq;
 using System.Text;
 using Verse;
 using UnityEngine;
+using RimWorld;
 
 namespace D9Framework
 {
     /// <summary>
     /// Abstract class you can extend to make Orbital Trade Beacons using the orbital trade hook.
     /// </summary>
-    public abstract class Building_OrbitalTradeBeacon : Building
+    public abstract class Building_CustomOrbitalTradeBeacon : Building_OrbitalTradeBeacon
     {
-        public abstract IEnumerable<IntVec3> TradeableCells();
+        // DO NOT call base.TradeableCells() in an override here! Because of how the patch works, an infinite loop would result. This is bad, and you shouldn't do it.
+        public new abstract IEnumerable<IntVec3> TradeableCells();
     }
 }
--- Source/D9Framework/Harmony/Orbital Trade Hook/OrbitalTradeHook.cs
+++ Source/D9Framework/Harmony/Orbital Trade Hook/OrbitalTradeHook.cs
@@ -13,31 +13,19 @@ namespace D9Framework
     /// <summary>
     /// Hook allowing modders to easily create custom trade beacons by extending <c>D9Framework.Building_OrbitalTradeBeacon</c>
     /// </summary>
-    // TODO: convert to transpilers smh
     [StaticConstructorOnStartup]
     public class OrbitalTradeHook
     {
         static MethodInfo RWTradeBeaconAllPowered, PatchAllPowered, RWTradeUtilityAllLaunchable, PatchAllLaunchable;
         static OrbitalTradeHook()
         {
-            CacheMethods();
+            //CacheMethods();
             var harmony = HarmonyInstance.Create("com.dninemfive.d9framework.oth");
             harmony.PatchAll(Assembly.GetExecutingAssembly());
             ULog.DebugMessage("Orbital Trade Hook loaded.");
         }
 
-        static void CacheMethods()
-        {
-            RWTradeBeaconAllPowered =     AccessTools.Method(type: typeof(RimWorld.Building_OrbitalTradeBeacon), parameters: new Type[] { typeof(Map) }, name: nameof(RimWorld.Building_OrbitalTradeBeacon.AllPowered));
-            RWTradeUtilityAllLaunchable = AccessTools.Method(type: typeof(TradeUtility),                         parameters: new Type[] { typeof(Map) }, name: nameof(TradeUtility.AllLaunchableThingsForTrade));
-            WTradeUtilityAllTradeable =   AccessTools.Method(type: typeof(TradeUtility),                         parameters: new Type[] { typeof(Map) }, name: nameof(TradeUtility.AllTrade)
-            PatchAllPowered = AccessTools.Method(type: typeof(OrbitalTradeHook), parameters: new Type[] { typeof(Map) }, name: nameof(OrbitalTradeHook.AllPowered));
-            
-            PatchAllLaunchable = AccessTools.Method(type: typeof(OrbitalTradeHook), parameters: new Type[] { typeof(Map) }, name: nameof(OrbitalTradeHook.AllLaunchableThingsForTrade));
-            R
-        }
-
-        public static IEnumerable<Building> AllPowered(Map map)
+        /*public static IEnumerable<Building> AllPowered(Map map)
         {
             foreach (RimWorld.Building_OrbitalTradeBeacon b in RimWorld.Building_OrbitalTradeBeacon.AllPowered(map)) yield return b; 
             foreach(D9Framework.Building_OrbitalTradeBeacon b in map.listerBuildings.AllBuildingsColonistOfClass<D9Framework.Building_OrbitalTradeBeacon>())
@@ -46,160 +34,27 @@ namespace D9Framework
                 CompRefuelable fuel = b.GetComp<CompRefuelable>();
                 if ((power == null || (power != null && power.PowerOn)) && (fuel == null || (fuel != null && fuel.HasFuel))) yield return b;                
             }
-        }
-
-        public static IEnumerable<IntVec3> AllTradeableCells(Map map)
-        {
-            foreach(Building b in AllPowered(map))
-            {
-                RimWorld.Building_OrbitalTradeBeacon rb = (RimWorld.Building_OrbitalTradeBeacon)b;
-                if (rb != null) foreach (IntVec3 cell in rb.TradeableCells) yield return cell;
-                D9Framework.Building_OrbitalTradeBeacon ob = b as D9Framework.Building_OrbitalTradeBeacon;
-                if (ob != null) foreach (IntVec3 cell in ob.TradeableCells()) yield return cell;
-            }
-        }
-
-        public static IEnumerable<Thing> AllLaunchableThingsForTrade(Map map)
-        {
-            HashSet<Thing> yielded = new HashSet<Thing>();
-            foreach (IntVec3 cell in AllTradeableCells(map))
-            {
-                List<Thing> thingList = cell.GetThingList(map);
-                for (int i = 0; i < thingList.Count(); i++)
-                {
-                    Thing t = thingList[i];
-                    if (t.def.category == ThingCategory.Item && TradeUtility.PlayerSellableNow(t) && !yielded.Contains(t))
-                    {
-                        yielded.Add(t);
-                        yield return t;
-                    }
-                }
-            }
-        }
-        /*[HarmonyPatch(typeof(RimWorld.Building_OrbitalTradeBeacon))]
-        [HarmonyPatch("TradeableCellsAround")]
-        [HarmonyPatch(new Type[] { typeof(Map) })]
-        class AllPoweredHook
-        {
-            [HarmonyPrefix]
-            public static bool TradeableCellsAroundPrefix()
-            {
-
-            }
         }*/
-        [HarmonyPatch(typeof(PassingShip))]
-        [HarmonyPatch("CommFloatMenuOption")]
-        [HarmonyPatch(new Type[] { typeof(Building_CommsConsole), typeof(Pawn) })]
-        class CommFloatMenuOptionHook
-        {
-            /*[HarmonyPrefix]
-            public static bool CommFloatMenuOptionPrefix(Building_CommsConsole console, Pawn negotiator, PassingShip __instance, ref FloatMenuOption __result)
-            {
-                string label = "CallOnRadio".Translate(__instance.GetCallLabel());
-                Action action = delegate
-                {
-                    if (!AllPowered(__instance.Map).Any())
-                    {
-                        Messages.Message("MessageNeedBeaconToTradeWithShip".Translate(), console, MessageTypeDefOf.RejectInput, false);
-                    }
-                    else
-                    {
-                        console.GiveUseCommsJob(negotiator, __instance);
-                    }
-                };
-                __result = FloatMenuUtility.DecoratePrioritizedTask(new FloatMenuOption(label, action, MenuOptionPriority.InitiateSocial, null, null, 0f, null, null), negotiator, console, "ReservedBy");
-                return false;
-            }*/
+        // transpile Building_OrbitalTradeBeacon.AllPowered to do the standard (fuel?.HasFuel || power?.Powered) check
 
-            // instead of a destructive prefix, just replace an IL call
-            // thanks to Smash Phil bc I used a Boats transpiler as an example
-            [HarmonyTranspiler]
-            public static IEnumerable<CodeInstruction> CommFloatMenuOptionTranspiler(IEnumerable<CodeInstruction> instr)
-            {
-                for(int i = 0; i < instr.Count(); i++)
-                {
-                    CodeInstruction ci = instr.ElementAt(i);
-                    if (ci.opcode == OpCodes.Call && ci.operand == RWTradeBeaconAllPowered)
-                    {
-                        yield return new CodeInstruction(opcode: OpCodes.Call, operand: PatchAllPowered);
-                    }
-                    else yield return ci;
-                }
-            }//end
-            
-        }
-        [HarmonyPatch(typeof(TradeUtility))]
-        [HarmonyPatch("AllLaunchableThingsForTrade")]
-        [HarmonyPatch(new Type[] { typeof(Map) })]
-        class AllLaunchableThingsForTradeHook
-        {
-            /*[HarmonyPrefix]
-            public static bool AllLaunchableThingsForTradePrefix(Map map, ref IEnumerable<Thing> __result)
-            {
-                __result = AllLaunchableThingsForTrade(map);
-                return false;
-            }*/
-            
-            [HarmonyTranspiler]
-            public static IEnumerable<CodeInstruction> CommFloatMenuOptionTranspiler(IEnumerable<CodeInstruction> instr)
-            {
-                for (int i = 0; i < instr.Count(); i++)
-                {
-                    CodeInstruction ci = instr.ElementAt(i);
-                    if (ci.opcode == OpCodes.Call && ci.operand == RWTradeUtilityAllLaunchable)
-                    {
-                        yield return new CodeInstruction(opcode: OpCodes.Call, operand: PatchAllLaunchable);
-                    }
-                    else yield return ci;
-                }
-            }//end
-        }
-        [HarmonyPatch(typeof(TradeUtility))]
-        [HarmonyPatch("LaunchThingsOfType")]
-        [HarmonyPatch(new Type[] { typeof(ThingDef), typeof(int), typeof(Map), typeof(TradeShip) })]
-        class LaunchThingsOfTypeHook
+        // thanks, lbmaian!
+        // from https://discordapp.com/channels/214523379766525963/215496692047413249/669060018645237800
+        [HarmonyPatch(typeof(Building_OrbitalTradeBeacon), nameof(Building_OrbitalTradeBeacon.TradeableCells))]
+        static class TradeableCellsPatch
         {
             [HarmonyPrefix]
-            public static bool LaunchThingsOfTypePrefix(ThingDef resDef, int debt, Map map, TradeShip trader)
+            static bool Prefix(Building_OrbitalTradeBeacon __instance, ref IEnumerable<IntVec3> __result)
             {
-                return false;
-            }
-            [HarmonyPostfix]
-            public static void LaunchThingsOfTypePostfix(ThingDef resDef, int debt, Map map, TradeShip trader)
-            {
-                while (debt > 0)
+                if (__instance is Building_CustomOrbitalTradeBeacon inst)
                 {
-                    Thing thing = null;
-                    foreach (IntVec3 current2 in AllTradeableCells(map))
-                    {
-                        foreach (Thing current3 in map.thingGrid.ThingsAt(current2))
-                        {
-                            if (current3.def == resDef)
-                            {
-                                thing = current3;
-                                goto IL_CC;
-                            }
-                        }
-                    }
-                    IL_CC:
-                    if (thing == null)
-                    {
-                        ULog.Error("Orbital Trade Hook: Could not find any " + resDef + " to transfer to trader.", false);
-                        break;
-                    }
-                    int num = Math.Min(debt, thing.stackCount);
-                    if (trader != null)
-                    {
-                        trader.GiveSoldThingToTrader(thing, num, TradeSession.playerNegotiator);
-                    }
-                    else
-                    {
-                        thing.SplitOff(num).Destroy(DestroyMode.Vanish);
-                    }
-                    debt -= num;
+                    __result = inst.TradeableCells();
+                    return false;
                 }
+                return true;
             }
-            //TODO: patch DropCellFinder.TradeDropSpot() to find any cell near either base or this OrbitalTradeBeacon. Not super high priority since it always returns something, but would be nice
         }
+
+        //TODO: patch DropCellFinder.TradeDropSpot() to find any cell near either base or this OrbitalTradeBeacon. Not super high priority since it always returns something, but would be nice
+    }
     }
 }
