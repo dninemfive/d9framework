b419240cc4222704e40c722ba4fd6d9c420a2ec6
dninemfive <11084010+dninemfive@users.noreply.github.com>
2020-01-20 21:25:01 -0800

    another transpiler done:tm:

--- Source/D9Framework/Harmony/Orbital Trade Hook/OrbitalTradeHook.cs
+++ Source/D9Framework/Harmony/Orbital Trade Hook/OrbitalTradeHook.cs
@@ -17,19 +17,21 @@ namespace D9Framework
     [StaticConstructorOnStartup]
     public class OrbitalTradeHook
     {
-        static MethodInfo RWTradeBeaconAllPowered, PatchAllPowered;
+        static MethodInfo RWTradeBeaconAllPowered, PatchAllPowered, RWTradeUtilityAllLaunchable, PatchAllLaunchable;
         static OrbitalTradeHook()
         {
             CacheMethods();
             var harmony = HarmonyInstance.Create("com.dninemfive.d9framework.oth");
             harmony.PatchAll(Assembly.GetExecutingAssembly());
-            ULog.DebugMessage("Orbital Trade Hook loaded.");            
+            ULog.DebugMessage("Orbital Trade Hook loaded.");
         }
 
         static void CacheMethods()
         {
-            RWTradeBeaconAllPowered = AccessTools.Method(type: typeof(RimWorld.Building_OrbitalTradeBeacon), parameters: new Type[] { typeof(Map) }, name: nameof(RimWorld.Building_OrbitalTradeBeacon.AllPowered));
+            RWTradeBeaconAllPowered =     AccessTools.Method(type: typeof(RimWorld.Building_OrbitalTradeBeacon), parameters: new Type[] { typeof(Map) }, name: nameof(RimWorld.Building_OrbitalTradeBeacon.AllPowered));
             PatchAllPowered = AccessTools.Method(type: typeof(OrbitalTradeHook), parameters: new Type[] { typeof(Map) }, name: nameof(OrbitalTradeHook.AllPowered));
+            RWTradeUtilityAllLaunchable = AccessTools.Method(type: typeof(TradeUtility),                         parameters: new Type[] { typeof(Map) }, name: nameof(TradeUtility.AllLaunchableThingsForTrade));
+            PatchAllLaunchable = AccessTools.Method(type: typeof(OrbitalTradeHook), parameters: new Type[] { typeof(Map) }, name: nameof(OrbitalTradeHook.AllLaunchableThingsForTrade));
         }
 
         public static IEnumerable<Building> AllPowered(Map map)
@@ -53,6 +55,35 @@ namespace D9Framework
                 if (ob != null) foreach (IntVec3 cell in ob.TradeableCells()) yield return cell;
             }
         }
+
+        public static IEnumerable<Thing> AllLaunchableThingsForTrade(Map map)
+        {
+            HashSet<Thing> yielded = new HashSet<Thing>();
+            foreach (IntVec3 cell in AllTradeableCells(map))
+            {
+                List<Thing> thingList = cell.GetThingList(map);
+                for (int i = 0; i < thingList.Count(); i++)
+                {
+                    Thing t = thingList[i];
+                    if (t.def.category == ThingCategory.Item && TradeUtility.PlayerSellableNow(t) && !yielded.Contains(t))
+                    {
+                        yielded.Add(t);
+                        yield return t;
+                    }
+                }
+            }
+        }
+        /*[HarmonyPatch(typeof(RimWorld.Building_OrbitalTradeBeacon))]
+        [HarmonyPatch("TradeableCellsAround")]
+        [HarmonyPatch(new Type[] { typeof(Map) })]
+        class AllPoweredHook
+        {
+            [HarmonyPrefix]
+            public static bool TradeableCellsAroundPrefix()
+            {
+
+            }
+        }*/
         [HarmonyPatch(typeof(PassingShip))]
         [HarmonyPatch("CommFloatMenuOption")]
         [HarmonyPatch(new Type[] { typeof(Building_CommsConsole), typeof(Pawn) })]
@@ -77,7 +108,7 @@ namespace D9Framework
                 return false;
             }*/
 
-            // instead of a destructive prefix, just replace two IL calls
+            // instead of a destructive prefix, just replace an IL call
             // thanks to Smash Phil bc I used a Boats transpiler as an example
             [HarmonyTranspiler]
             public static IEnumerable<CodeInstruction> CommFloatMenuOptionTranspiler(IEnumerable<CodeInstruction> instr)
@@ -99,29 +130,26 @@ namespace D9Framework
         [HarmonyPatch(new Type[] { typeof(Map) })]
         class AllLaunchableThingsForTradeHook
         {
-            [HarmonyPrefix]
+            /*[HarmonyPrefix]
             public static bool AllLaunchableThingsForTradePrefix(Map map, ref IEnumerable<Thing> __result)
             {
                 __result = AllLaunchableThingsForTrade(map);
                 return false;
-            }
-            private static IEnumerable<Thing> AllLaunchableThingsForTrade(Map map)
+            }*/
+            
+            [HarmonyTranspiler]
+            public static IEnumerable<CodeInstruction> CommFloatMenuOptionTranspiler(IEnumerable<CodeInstruction> instr)
             {
-                HashSet<Thing> yielded = new HashSet<Thing>();
-                foreach (IntVec3 cell in AllTradeableCells(map))
+                for (int i = 0; i < instr.Count(); i++)
                 {
-                    List<Thing> thingList = cell.GetThingList(map);
-                    for (int i = 0; i < thingList.Count(); i++)
+                    CodeInstruction ci = instr.ElementAt(i);
+                    if (ci.opcode == OpCodes.Call && ci.operand == RWTradeUtilityAllLaunchable)
                     {
-                        Thing t = thingList[i];
-                        if (t.def.category == ThingCategory.Item && TradeUtility.PlayerSellableNow(t) && !yielded.Contains(t))
-                        {
-                            yielded.Add(t);
-                            yield return t;
-                        }
+                        yield return new CodeInstruction(opcode: OpCodes.Call, operand: PatchAllLaunchable);
                     }
+                    else yield return ci;
                 }
-            }
+            }//end
         }
         [HarmonyPatch(typeof(TradeUtility))]
         [HarmonyPatch("LaunchThingsOfType")]
