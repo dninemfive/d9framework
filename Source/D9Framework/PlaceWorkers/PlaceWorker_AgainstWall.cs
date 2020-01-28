82ecda592628104bdb9c9ce3212d83763144f7bd
dninemfive <11084010+dninemfive@users.noreply.github.com>
2020-01-27 22:37:51 -0800

    add PlaceWorkers from AJO, additionally by CuproPanda

---
+++ Source/D9Framework/PlaceWorkers/PlaceWorker_AgainstWall.cs
@@ -0,0 +1,44 @@
+﻿using RimWorld;
+using Verse;
+
+namespace AdditionalJoyObjects {
+  /// <summary>
+  /// Only allows an object to be placed against a wall
+  /// <para>(Checks the cell behind this object)</para>
+  /// </summary>
+  public class PlaceWorker_AlongsideWall : PlaceWorker {
+
+
+		public override AcceptanceReport AllowsPlacing(BuildableDef checkingDef, IntVec3 loc, Rot4 rot, Map map, Thing thingToIgnore = null) {
+
+      // Get the tile behind this object
+      IntVec3 c = loc - rot.FacingCell;
+      // Determine if the tile is an edifice
+      Building edifice = c.GetEdifice(map);
+
+      // Don't place outside of the map
+      if (!c.InBounds(map) || !loc.InBounds(map)) {
+        return false;
+      }
+
+        /*  // Only allow placing on a natural or constructed wall
+        if (edifice == null || edifice.def == null || (edifice.def != ThingDefOf.Wall && !edifice.def.building.isNaturalRock &&
+            ((edifice.Faction == null || edifice.Faction != Faction.OfPlayer) ||
+            edifice.def.graphicData == null || edifice.def.graphicData.linkFlags == 0 || (LinkFlags.Wall & edifice.def.graphicData.linkFlags) == LinkFlags.None))) {
+            return new AcceptanceReport("AJO_MustBePlacedOnWall".Translate(new object[] { checkingDef.LabelCap }));
+        }
+        */
+
+        if (edifice == null || edifice.def == null || ((edifice.def != ThingDefOf.Wall && !edifice.def.IsSmoothed) &&
+            ((edifice.Faction == null || edifice.Faction != Faction.OfPlayer) ||
+            edifice.def.graphicData == null || edifice.def.graphicData.linkFlags == 0 || (LinkFlags.Wall & edifice.def.graphicData.linkFlags) == LinkFlags.None)))
+            {
+            return new AcceptanceReport("AJO_MustBePlacedOnWall".Translate(new object[] { checkingDef.LabelCap }));
+        }
+
+        // Otherwise, accept placing
+        return true;
+    }
+  }
+}
+
---
+++ Source/D9Framework/PlaceWorkers/PlaceWorker_Chandelier.cs
@@ -0,0 +1,25 @@
+﻿using Verse;
+
+namespace AdditionalJoyObjects {
+
+  internal class PlaceWorker_Chandelier : PlaceWorker {
+
+
+		public override AcceptanceReport AllowsPlacing(BuildableDef checkingDef, IntVec3 loc, Rot4 rot, Map map, Thing thingToIgnore = null) {
+      // Don't allow placing on big things
+      foreach (IntVec3 c in GenAdj.CellsOccupiedBy(loc, rot, checkingDef.Size)) {
+        if (c.GetEdifice(map) != null) {
+          if (c.GetEdifice(map).def.blockWind == true || c.GetEdifice(map).def.holdsRoof == true) {
+            return new AcceptanceReport("AJO_ObjectTooTall".Translate(new object[] { c.GetEdifice(map).LabelCap, checkingDef.LabelCap }));
+          }
+        }
+        if (c.GetThingList(map).Find(ch => ch is Building_Chandelier) != null) {
+          return new AcceptanceReport("IdenticalThingExists".Translate());
+        }
+      }
+
+      // Otherwise, accept placing
+      return true;
+    }
+  }
+}
