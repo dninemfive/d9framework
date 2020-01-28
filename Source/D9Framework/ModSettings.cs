4d2e1b8e6c975420412442828cc0e9e03e9dac25
dninemfive <11084010+dninemfive@users.noreply.github.com>
2020-01-27 20:32:17 -0800

    mod settings for conditional patch application added

--- Source/D9Framework/ModSettings.cs
+++ Source/D9Framework/ModSettings.cs
@@ -10,13 +10,19 @@ namespace D9Framework
     public class D9FModSettings : ModSettings
     {
         public static bool DEBUG = true; //for release set false by default
-        //TODO: add options to disable each individual Harmony patch iff DEBUG is true
-        //make it so the patch isn't even added to the game, game must be reloaded to take effect
+
+        public static bool ApplyCompFromStuff => !DEBUG || applyCFS;
+        public static bool ApplyOrbitalTradeHook => !DEBUG || applyOTH;
+        public static bool ApplyDeconstructReturnFix = !DEBUG || applyDRF;
+        public static bool applyCFS = true, applyOTH = true, applyDRF = true; // despite the public flag, don't reference these; they're only public for the purposes of the mod settings screen below. Reference the above variables instead.
 
         public override void ExposeData()
         {
             base.ExposeData();
             Scribe_Values.Look(ref DEBUG, "debug", false);
+            Scribe_Values.Look(ref applyCFS, "ApplyCompFromStuff", true);
+            Scribe_Values.Look(ref applyOTH, "ApplyOrbitalTradeHook", true);
+            Scribe_Values.Look(ref applyDRF, "ApplyDeconstructReturnFix", true);
         }
     }
     public class D9FrameworkMod : Mod
@@ -32,6 +38,12 @@ namespace D9Framework
             Listing_Standard listing = new Listing_Standard();
             listing.Begin(inRect);
             listing.CheckboxLabeled("D9FSettingsDebug".Translate(), ref D9FModSettings.DEBUG, "D9FSettingsDebugTooltip".Translate());
+            if (D9FModSettings.DEBUG)
+            {
+                listing.CheckboxLabeled("D9FSettingsApplyCFS".Translate(), ref D9FModSettings.applyCFS, "D9FSettingsApplyCFSTooltip".Translate());
+                listing.CheckboxLabeled("D9FSettingsApplyOTH".Translate(), ref D9FModSettings.applyOTH, "D9FSettingsApplyOTHTooltip".Translate());
+                listing.CheckboxLabeled("D9FSettingsApplyDRF".Translate(), ref D9FModSettings.applyDRF, "D9FSettingsApplyDRFTooltip".Translate());
+            }
             listing.End();
             base.DoSettingsWindowContents(inRect);
         }
