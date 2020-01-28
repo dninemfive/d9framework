55aa1f47c87f57007f743cfda5c10b7871fea159
dninemfive <11084010+dninemfive@users.noreply.github.com>
2020-01-27 22:48:09 -0800

    format said PlaceWorkers

---
+++ Source/D9Framework/PlaceWorkers/PlaceWorker_AlongsideWall.cs
@@ -0,0 +1,35 @@
+﻿using RimWorld;
+using Verse;
+
+namespace D9Framework
+{
+    /// <summary>
+    /// Only allows an object to be placed against a wall
+    /// <para>(Checks the cell behind this object)</para>
+    /// <para>Originally by CuproPanda, for Additional Joy Objects.</para>
+    /// </summary>
+    public class PlaceWorker_AlongsideWall : PlaceWorker
+    {
+		public override AcceptanceReport AllowsPlacing(BuildableDef checkingDef, IntVec3 loc, Rot4 rot, Map map, Thing thingToIgnore = null)
+        {        
+            IntVec3 c = loc - rot.FacingCell;                         // Get the tile behind this object
+            Building edifice = c.GetEdifice(map);                     // Determine if the tile is an edifice            
+            if (!c.InBounds(map) || !loc.InBounds(map)) return false; // Don't place outside of the map
+            /*  // Only allow placing on a natural or constructed wall
+            if (edifice == null || edifice.def == null || (edifice.def != ThingDefOf.Wall && !edifice.def.building.isNaturalRock &&
+                ((edifice.Faction == null || edifice.Faction != Faction.OfPlayer) ||
+                edifice.def.graphicData == null || edifice.def.graphicData.linkFlags == 0 || (LinkFlags.Wall & edifice.def.graphicData.linkFlags) == LinkFlags.None))) {
+                return new AcceptanceReport("AJO_MustBePlacedOnWall".Translate(new object[] { checkingDef.LabelCap }));
+            }
+            */
+            if (edifice == null || 
+                edifice.def == null || 
+                ((edifice.def != ThingDefOf.Wall && !edifice.def.IsSmoothed) && 
+                ((edifice.Faction == null || edifice.Faction != Faction.OfPlayer) ||
+                edifice.def.graphicData == null || edifice.def.graphicData.linkFlags == 0 || (LinkFlags.Wall & edifice.def.graphicData.linkFlags) == LinkFlags.None)))
+                    return new AcceptanceReport("AJO_MustBePlacedOnWall".Translate(checkingDef.LabelCap));        
+            return true; // Otherwise, accept placing
+        }
+  }
+}
+
--- Source/D9Framework/PlaceWorkers/PlaceWorker_Roofed.cs
+++ Source/D9Framework/PlaceWorkers/PlaceWorker_Roofed.cs
@@ -1,20 +1,17 @@
 ﻿using Verse;
 
 namespace AdditionalJoyObjects {
-  /// <summary>
-  /// Restrict buildings to being placed under a roof
-  /// </summary>
-  public class PlaceWorker_Roofed : PlaceWorker {
-    /// <summary></summary>
-    public override AcceptanceReport AllowsPlacing(BuildableDef checkingDef, IntVec3 loc, Rot4 rot, Map map, Thing thingToIgnore = null) {
-
-      foreach (IntVec3 current in GenAdj.CellsOccupiedBy(loc, rot, checkingDef.Size)) {
-        if (!map.roofGrid.Roofed(current)) {
-          return new AcceptanceReport ("AJO_NeedsRoof".Translate(new object[] { checkingDef.LabelCap }));
+    /// <summary>
+    /// Restrict buildings to being placed under a roof
+    /// </summary>
+    public class PlaceWorker_Roofed : PlaceWorker {
+        public override AcceptanceReport AllowsPlacing(BuildableDef checkingDef, IntVec3 loc, Rot4 rot, Map map, Thing thingToIgnore = null) {
+            foreach (IntVec3 current in GenAdj.CellsOccupiedBy(loc, rot, checkingDef.Size)) {
+                if (!map.roofGrid.Roofed(current)) {
+                    return new AcceptanceReport ("AJO_NeedsRoof".Translate(checkingDef.label));
+                }
+            }
+            return true;
         }
-      }
-
-      return true;
     }
-  }
 }
