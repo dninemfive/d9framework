cccb9c3228a9683e0bbc83ea008f24aad2c0fe42
dninemfive <11084010+dninemfive@users.noreply.github.com>
2020-01-28 13:11:19 -0800

    make roofed PlaceWorkers work, add keyed strings therefor

--- Source/D9Framework/PlaceWorkers/PlaceWorker_AgainstWall.cs
+++ Source/D9Framework/PlaceWorkers/PlaceWorker_AgainstWall.cs
@@ -4,14 +4,13 @@ using Verse;
 namespace D9Framework
 {
     /// <summary>
-    /// Only allows an object to be placed against a wall
-    /// <para>(Checks the cell behind this object)</para>
+    /// Only allows an object to be placed against a wall (Checks the cell behind this object)
     /// <para>Originally by CuproPanda, for Additional Joy Objects.</para>
     /// </summary>
-    public class PlaceWorker_AlongsideWall : PlaceWorker
+    public class PlaceWorker_AgainstWall : PlaceWorker
     {
-		public override AcceptanceReport AllowsPlacing(BuildableDef checkingDef, IntVec3 loc, Rot4 rot, Map map, Thing thingToIgnore = null)
-        {        
+        public override AcceptanceReport AllowsPlacing(BuildableDef checkingDef, IntVec3 loc, Rot4 rot, Map map, Thing thingToIgnore = null)
+        {
             IntVec3 c = loc - rot.FacingCell;                         // Get the tile behind this object
             Building edifice = c.GetEdifice(map);                     // Determine if the tile is an edifice            
             if (!c.InBounds(map) || !loc.InBounds(map)) return false; // Don't place outside of the map
@@ -22,14 +21,18 @@ namespace D9Framework
                 return new AcceptanceReport("AJO_MustBePlacedOnWall".Translate(new object[] { checkingDef.LabelCap }));
             }
             */
-            if (edifice == null || 
-                edifice.def == null || 
-                ((edifice.def != ThingDefOf.Wall && !edifice.def.IsSmoothed) && 
+            if (edifice == null ||
+                edifice.def == null ||
+                ((edifice.def != ThingDefOf.Wall && !edifice.def.IsSmoothed) &&
                 ((edifice.Faction == null || edifice.Faction != Faction.OfPlayer) ||
                 edifice.def.graphicData == null || edifice.def.graphicData.linkFlags == 0 || (LinkFlags.Wall & edifice.def.graphicData.linkFlags) == LinkFlags.None)))
-                    return new AcceptanceReport("AJO_MustBePlacedOnWall".Translate(checkingDef.LabelCap));        
+                return new AcceptanceReport("AJO_MustBePlacedOnWall".Translate(checkingDef.LabelCap));
             return true; // Otherwise, accept placing
         }
-  }
+    }
+    public class PlaceWorker_OnWall : PlaceWorker
+    {
+        // on wall and not on (the same ThingDef facing the same way)
+    }
 }
 
--- Source/D9Framework/PlaceWorkers/PlaceWorker_Roofed.cs
+++ Source/D9Framework/PlaceWorkers/PlaceWorker_Roofed.cs
@@ -1,16 +1,54 @@
-﻿using Verse;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
 
-namespace AdditionalJoyObjects {
+namespace D9Framework
+{
     /// <summary>
     /// Restrict buildings to being placed under a roof
     /// </summary>
-    public class PlaceWorker_Roofed : PlaceWorker {
-        public override AcceptanceReport AllowsPlacing(BuildableDef checkingDef, IntVec3 loc, Rot4 rot, Map map, Thing thingToIgnore = null) {
-            foreach (IntVec3 current in GenAdj.CellsOccupiedBy(loc, rot, checkingDef.Size)) {
-                if (!map.roofGrid.Roofed(current)) {
-                    return new AcceptanceReport ("AJO_NeedsRoof".Translate(checkingDef.label));
+    /// <para>Originally by CuproPanda, for Additional Joy Objects.</para>
+    public class PlaceWorker_Roofed : PlaceWorker
+    {
+        public override AcceptanceReport AllowsPlacing(BuildableDef checkingDef, IntVec3 loc, Rot4 rot, Map map, Thing thingToIgnore = null)
+        {
+            foreach (IntVec3 current in GenAdj.CellsOccupiedBy(loc, rot, checkingDef.Size))
+            {
+                if (!map.roofGrid.Roofed(current))
+                {
+                    return new AcceptanceReport("D9F_Roofed_NeedsRoof".Translate(checkingDef.label));
                 }
             }
+            return true;
+        }
+    }
+    /// <summary>
+    /// Require that the building be placed "hanging" from a roof, i.e. that the tile is roofed, it's not above anything which touches the roof, and it's not on the same tile as another RoofHanger
+    /// </summary>
+    /// <para>Originally by CuproPanda, for Additional Joy Objects.</para>
+    public class PlaceWorker_RoofHanger : PlaceWorker_Roofed
+    {
+		public override AcceptanceReport AllowsPlacing(BuildableDef checkingDef, IntVec3 loc, Rot4 rot, Map map, Thing thingToIgnore = null) {
+            AcceptanceReport roofedReport = base.AllowsPlacing(checkingDef, loc, rot, map, thingToIgnore); //check if tile is roofed
+            if (!roofedReport.Accepted) return roofedReport;            
+            foreach (IntVec3 c in GenAdj.CellsOccupiedBy(loc, rot, checkingDef.Size)) // Don't allow placing on big things
+            {
+                if (c.GetEdifice(map) != null)
+                {
+                    if (c.GetEdifice(map).def.blockWind == true || c.GetEdifice(map).def.holdsRoof == true)
+                    {
+                        return new AcceptanceReport("D9F_Chandelier_TooTall".Translate(c.GetEdifice(map).LabelCap, checkingDef.LabelCap));
+                    }
+                }
+                IEnumerable<Thing> things = c.GetThingList(map);
+                if (things.Where(x => x.def.placeWorkers.Where(y => y.GetType() == typeof(PlaceWorker_RoofHanger)).Any()).Any()) // don't hang if there's already a chandelier here
+                {
+                    return new AcceptanceReport("IdenticalThingExists".Translate());
+                }
+            }
+            // Otherwise, accept placing
             return true;
         }
     }
