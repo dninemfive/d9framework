cd17df2d9f7dbf0dcec73a4eda02a0dc125c6c6f
dninemfive <11084010+dninemfive@users.noreply.github.com>
2020-01-29 10:53:06 -0800

    add PlaceWorker_OnWall

--- Source/D9Framework/PlaceWorkers/PlaceWorker_AgainstWall.cs
+++ Source/D9Framework/PlaceWorkers/PlaceWorker_AgainstWall.cs
@@ -1,4 +1,5 @@
-﻿using RimWorld;
+﻿using System.Collections.Generic;
+using RimWorld;
 using Verse;
 
 namespace D9Framework
@@ -24,9 +25,30 @@ namespace D9Framework
             return def != null && (def.holdsRoof && def.blockLight && def.coversFloor);
         }
     }
+    /// <summary>
+    /// Only allows an object to be placed on a wall, as long as there isn't the same def facing the same way.
+    /// </summary>
     public class PlaceWorker_OnWall : PlaceWorker
     {
-        // on wall and not on (the same ThingDef facing the same way)
+        public override AcceptanceReport AllowsPlacing(BuildableDef checkingDef, IntVec3 loc, Rot4 rot, Map map, Thing thingToIgnore = null)
+        {
+            Building buil = loc.GetEdifice(map);
+            if (!loc.InBounds(map)) return false;
+            if (!IsWall(buil) || buil.Faction != Faction.OfPlayer) return new AcceptanceReport("D9F_MustBePlacedOnWall".Translate(checkingDef.LabelCap));
+            if (ConflictingThing(checkingDef, loc, rot, map)) return new AcceptanceReport("IdenticalThingExists".Translate());
+            return true;
+        }
+        public bool IsWall(Building b)
+        {
+            ThingDef def = b?.def;
+            return def != null && (def.holdsRoof && def.blockLight && def.coversFloor);
+        }
+        public bool ConflictingThing(BuildableDef bd, IntVec3 c, Rot4 r, Map map)
+        {
+            List<Thing> things = map.thingGrid.ThingsListAtFast(c);
+            foreach (Thing t in things) if (t.def as BuildableDef == bd && t.Rotation == r) return false;
+            return true;
+        }
     }
 }
 
