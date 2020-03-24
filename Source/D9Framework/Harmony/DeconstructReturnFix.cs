047b38ced6fdf64e85c22f65f50c39e00fe2dadb
dninemfive <11084010+dninemfive@users.noreply.github.com>
2020-03-23 17:06:19 -0700

    refactor, fix(?) Deconstruct Return Fix

--- Source/D9Framework/Harmony/DeconstructReturnFix.cs
+++ Source/D9Framework/Harmony/DeconstructReturnFix.cs
@@ -16,99 +16,77 @@ namespace D9Framework
     [StaticConstructorOnStartup]
     static class DeconstructReturnFix
     {
-        [HarmonyPatch(typeof(GenLeaving), "DoLeavingsFor", new Type[] { typeof(Thing), typeof(Map), typeof(DestroyMode), typeof(CellRect), typeof(Predicate<IntVec3>)})]
+        [HarmonyPatch(typeof(GenLeaving), "DoLeavingsFor", new Type[] { typeof(Thing), typeof(Map), typeof(DestroyMode), typeof(CellRect), typeof(Predicate<IntVec3>), typeof(List<Thing>)})]
         class CalcFix
         {
             [HarmonyPrefix]
             public static bool DoLeavingsForPrefix(Thing diedThing, Map map, DestroyMode mode, CellRect leavingsRect, Predicate<IntVec3> nearPlaceValidator = null)
             {
-                //code copied and modified directly from DoLeavingsFor
-                if (mode == DestroyMode.Deconstruct)// || mode == DestroyMode.Refund)
-                {
-                    ThingOwner<Thing> thingOwner = new ThingOwner<Thing>();
-                    if (GenLeaving.CanBuildingLeaveResources(diedThing, mode))
-                    {                        
-                        Frame frame = diedThing as Frame;
-                        if (frame != null)
-                        {
-                            for (int num = frame.resourceContainer.Count - 1; num >= 0; num--)
-                            {                                
-                                int num2 = GBRLC(diedThing, mode)(frame.resourceContainer[num].stackCount);
-                                if (num2 > 0)
-                                {
-                                    frame.resourceContainer.TryTransferToContainer(frame.resourceContainer[num], thingOwner, num2, true);
-                                }
-                            }
-                            frame.resourceContainer.ClearAndDestroyContents(mode);
-                        }
-                        else
-                        {
-                            List<ThingDefCountClass> list = diedThing.CostListAdjusted();
-                            for (int l = 0; l < list.Count; l++)
-                            {                                
-                                ThingDefCountClass thingDefCountClass = list[l];
-                                int num3 = GBRLC(diedThing, mode)(thingDefCountClass.count);
-                                if (num3 > 0)
-                                {
-                                    Thing thing2 = ThingMaker.MakeThing(thingDefCountClass.thingDef, null);
-                                    thing2.stackCount = num3;
-                                    thingOwner.TryAdd(thing2, true);
-                                }
-                            }//end for
-                        }//end if..else
-                    }//end if
-                    List<IntVec3> list2 = leavingsRect.Cells.InRandomOrder(null).ToList();
-                    int num5 = 0;
-                    while (true)
+                if (mode != DestroyMode.Deconstruct) return true; //just to make it more readable
+                ThingOwner<Thing> thingOwner = new ThingOwner<Thing>();
+                if (GenLeaving.CanBuildingLeaveResources(diedThing, mode))
+                {                        
+                    Frame frame = diedThing as Frame;
+                    if (frame != null)
                     {
-                        if (thingOwner.Count > 0)
-                        {
-                            ThingOwner<Thing> thingOwner2 = thingOwner;
-                            Thing thing3 = thingOwner[0];
-                            IntVec3 dropLoc = list2[num5];
-                            ThingPlaceMode mode2 = ThingPlaceMode.Near;
-                            Thing thing4 = default(Thing);
-                            if (thingOwner2.TryDrop(thing3, dropLoc, map, mode2, out thing4, null, nearPlaceValidator))
-                            {
-                                num5++;
-                                if (num5 >= list2.Count)
-                                {
-                                    num5 = 0;
-                                }
-                                continue;
-                            }
-                            break;
+                        for (int frameResCt = frame.resourceContainer.Count - 1; frameResCt >= 0; frameResCt--)
+                        {                                
+                            int gblrc;
+                            if ((gblrc = GBRLC(diedThing)(frame.resourceContainer[frameResCt].stackCount)) > 0) frame.resourceContainer.TryTransferToContainer(frame.resourceContainer[frameResCt], thingOwner, gblrc, true);
                         }
-                        return false;
+                        frame.resourceContainer.ClearAndDestroyContents(mode);
                     }
-                    ULog.Warning("Deconstruct Return Fix: Failed to place all leavings for destroyed thing " + diedThing + " at " + leavingsRect.CenterCell, false);
+                    else
+                    {
+                        // TODO: ModExtension specifying drop rates per ThingDef. Needs to be relatively optimized.
+                        List<ThingDefCountClass> list = diedThing.CostListAdjusted();
+                        for (int l = 0; l < list.Count; l++)
+                        {                                
+                            ThingDefCountClass tdcc = list[l];
+                            int gblrc;
+                            if ((gblrc = GBRLC(diedThing)(tdcc.count)) > 0)
+                            {
+                                Thing thing = ThingMaker.MakeThing(tdcc.thingDef, null);
+                                thing.stackCount = gblrc;
+                                thingOwner.TryAdd(thing, true);
+                            }
+                        }
+                    }
+                }
+                List<IntVec3> cellList = leavingsRect.Cells.InRandomOrder(null).ToList();
+                int cellInd = 0;
+                while (true)
+                {
+                    if (thingOwner.Count > 0)
+                    {
+                        ThingOwner<Thing> thingOwner2 = thingOwner;
+                        Thing thing = thingOwner[0];
+                        IntVec3 dropLoc = cellList[cellInd];
+                        ThingPlaceMode mode2 = ThingPlaceMode.Near;
+                        Thing thing4 = default(Thing);
+                        if (thingOwner2.TryDrop(thing, dropLoc, map, mode2, out thing4, null, nearPlaceValidator))
+                        {
+                            cellInd++;
+                            if (cellInd >= cellList.Count)
+                            {
+                                cellInd = 0;
+                            }
+                            continue;
+                        }
+                        break;
+                    }
                     return false;
-                }//end if
-                return true;
+                }
+                ULog.Warning("Deconstruct Return Fix: Failed to place all leavings for destroyed thing " + diedThing + " at " + leavingsRect.CenterCell, false);
+                return false;
             }//end DoLeavingsForPrefix
-            public static Func<int, int> GBRLC(Thing t, DestroyMode d) //GetBuildingResourcesLeaveCalculator, very slightly modified.
+            public static Func<int, int> GBRLC(Thing t)
             {
-                if (!GenLeaving.CanBuildingLeaveResources(t, d))
+                if (!GenLeaving.CanBuildingLeaveResources(t, DestroyMode.Deconstruct))
                 {
                     return (int count) => 0;
                 }
-                switch (d)
-                {
-                    case DestroyMode.Vanish:
-                        return (int count) => 0;
-                    case DestroyMode.KillFinalize:
-                        return (int count) => GenMath.RoundRandom((float)count * 0.5f);
-                    case DestroyMode.Deconstruct:
-                        return (int count) => GenMath.RoundRandom(Mathf.Min((float)count * t.def.resourcesFractionWhenDeconstructed, (float)(count)));
-                    case DestroyMode.Cancel:
-                        return (int count) => GenMath.RoundRandom((float)count * 1f);
-                    case DestroyMode.FailConstruction:
-                        return (int count) => GenMath.RoundRandom((float)count * 0.5f);
-                    case DestroyMode.Refund:
-                        return (int count) => count;
-                    default:
-                        throw new ArgumentException("Unknown destroy mode " + d);
-                }
+                return (int count) => GenMath.RoundRandom(Mathf.Min((float)count * t.def.resourcesFractionWhenDeconstructed, (float)(count))); //other destroy modes deleted because I always know the mode
             }
         }//end CalcFix
     }//end DeconstructReturnFix
