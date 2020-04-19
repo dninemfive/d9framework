f40910bf7d4b445ce388578c7749ac5dc41f4d35
dninemfive <11084010+dninemfive@users.noreply.github.com>
2020-04-19 16:52:33 -0700

    fix Force Allow Place Over Fix

--- Source/D9Framework/Harmony/ForceAllowPlaceOverFix.cs
+++ Source/D9Framework/Harmony/ForceAllowPlaceOverFix.cs
@@ -11,59 +11,78 @@ using RimWorld;
 
 namespace D9Framework
 {
+    /// <summary>
+    /// Makes BuildableDef.ForceAllowPlaceOver actually cause the appropriate things to ignore the defs in question
+    /// </summary>
     static class ForceAllowPlaceOverFix
     {
+        /// <summary>
+        /// Inserts the equivalent of "if newDef.ForceAllowPlaceOver(oldDef) return true;" into GenConstruct.CanPlaceBlueprintOver
+        /// </summary>
         [HarmonyPatch(typeof(GenConstruct), nameof(GenConstruct.CanPlaceBlueprintOver), new Type[] { typeof(BuildableDef), typeof(ThingDef) })]
         class GenConstructPlaceOver
-        {
+        {            
             [HarmonyTranspiler]
-            static IEnumerable<CodeInstruction> CanPlaceBlueprintOverTranspiler(IEnumerable<CodeInstruction> instructions)
+            static IEnumerable<CodeInstruction> CanPlaceBlueprintOverTranspiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
             {
                 List<CodeInstruction> instrList = instructions.ToList();
+                Label returnTrue = generator.DefineLabel();
                 for (int i = 0; i < instrList.Count; i++)
                 {
                     // Looking for the first half of (oldDef == ThingDefOf.SteamGeyser && !newDef.ForceAllowPlaceOver(oldDef))
-                    if (instrList[i].opcode == OpCodes.Ldarg_1                                                             // IL 0074: ldarg.1
-                        && instrList[i+1].opcode == OpCodes.Ldsfld                                                         // IL 0075: ldsfld class Verse.ThingDef RimWorld.ThingDefOf::SteamGeyser
-                        && instrList[i+1].operand as FieldInfo == AccessTools.Field(typeof(ThingDefOf), nameof(ThingDefOf.SteamGeyser))    
-                        && instrList[i+2].opcode == OpCodes.Bne_Un_S)                                                      // IL 007A: bne.un.s IL_0087
+                    if (instrList[i + 1].opcode == OpCodes.Ldsfld                                                         // IL 0075: ldsfld class Verse.ThingDef RimWorld.ThingDefOf::SteamGeyser
+                        && instrList[i + 1].operand as FieldInfo == AccessTools.Field(typeof(ThingDefOf), nameof(ThingDefOf.SteamGeyser)))
                     {
-                        //for (int j = 0; j < 3; j++) Log.Message(instrList[i + j].opcode.ToString());
-                        instrList[i].opcode   = OpCodes.Nop;
-                        instrList[i+1].opcode = OpCodes.Nop;
-                        instrList[i+2].opcode = OpCodes.Nop;
-                        //for (int j = 0; j < 3; j++) Log.Message(instrList[i + j].opcode.ToString() + " " + instrList[i+j].operand);
+                        // if newDef.ForceAllowPlaceOver(oldDef) return true;
+                        yield return new CodeInstruction(OpCodes.Ldarg_0); // load newDef onto the stack
+                        yield return new CodeInstruction(OpCodes.Ldarg_1); // load oldDef onto the stack
+                        // call newDef.ForceAllowPlaceOver
+                        yield return new CodeInstruction(OpCodes.Callvirt, AccessTools.Method(typeof(BuildableDef), nameof(BuildableDef.ForceAllowPlaceOver)));
+                        // if the above call leaves 1 on the stack, jump to my "return true" statement
+                        yield return new CodeInstruction(OpCodes.Brtrue, returnTrue);
                     }
+                    yield return instrList[i];
                 }
-                foreach (CodeInstruction ci in instrList) yield return ci;
+                // append a "return true" at the end with my label
+                CodeInstruction loadTrue = new CodeInstruction(OpCodes.Ldc_I4_1);
+                loadTrue.labels.Add(returnTrue);
+                yield return loadTrue;
+                yield return new CodeInstruction(OpCodes.Ret);
             }
         }
 
+        /// <summary>
+        /// Inserts the equivalent of "if newDef.ForceAllowPlaceOver(oldDef) return true;" into GenConstruct.BlocksConstruction
+        /// </summary>
         [HarmonyPatch(typeof(GenConstruct), nameof(GenConstruct.BlocksConstruction), new Type[] { typeof(Thing), typeof(Thing) })]
         class GenConstructBlocksConstruction
         {
             [HarmonyTranspiler]
-            static IEnumerable<CodeInstruction> CanPlaceBlueprintOverTranspiler(IEnumerable<CodeInstruction> instructions)
+            static IEnumerable<CodeInstruction> CanPlaceBlueprintOverTranspiler(IEnumerable<CodeInstruction> instructions, ILGenerator generator)
             {
                 List<CodeInstruction> instrList = instructions.ToList();
+                Label returnTrue = generator.DefineLabel();
                 for (int i = 0; i < instructions.Count(); i++)
                 {
                     // Looking for the first half of (t.def == ThingDefOf.SteamGeyser && thingDef.entityDefToBuild.ForceAllowPlaceOver(t.def))
-                    if (instrList[i].opcode == OpCodes.Ldarg_1                                                         // IL 00D1: ldarg.1
-                        && instrList[i+1].opcode == OpCodes.Ldfld                                                      // IL 00D2: ldfld class Verse.ThingDef Verse.Thing::def
-                        && instrList[i+2].opcode == OpCodes.Ldsfld                                                     // IL 00D7: ldsfld class Verse.ThingDef RimWorld.ThingDefOf::SteamGeyser
-                        && instrList[i+2].operand as FieldInfo == AccessTools.Field(typeof(ThingDefOf), nameof(ThingDefOf.SteamGeyser))
-                        && instrList[i+3].opcode == OpCodes.Bne_Un_S)                                                // IL 00DC: bne.un.s IL_0087
+                    if (instrList[i+2].opcode == OpCodes.Ldsfld                                                     // IL 00D7: ldsfld class Verse.ThingDef RimWorld.ThingDefOf::SteamGeyser
+                        && instrList[i+2].operand as FieldInfo == AccessTools.Field(typeof(ThingDefOf), nameof(ThingDefOf.SteamGeyser)))
                     {
-                        //for (int j = 0; j < 4; j++) Log.Message(instrList[i + j].opcode.ToString());
-                        instrList[i].opcode   = OpCodes.Nop;
-                        instrList[i+1].opcode = OpCodes.Nop;
-                        instrList[i+2].opcode = OpCodes.Nop;
-                        instrList[i+3].opcode = OpCodes.Nop;
-                        //for (int j = 0; j < 4; j++) Log.Message(instrList[i + j].opcode.ToString() + " " + instrList[i + j].operand);
+                        // if newDef.ForceAllowPlaceOver(oldDef) return true;
+                        yield return new CodeInstruction(OpCodes.Ldarg_0); // load newDef onto the stack
+                        yield return new CodeInstruction(OpCodes.Ldarg_1); // load oldDef onto the stack
+                        // call newDef.ForceAllowPlaceOver
+                        yield return new CodeInstruction(OpCodes.Callvirt, AccessTools.Method(typeof(BuildableDef), nameof(BuildableDef.ForceAllowPlaceOver)));
+                        // if the above call leaves 1 on the stack, jump to my "return true" statement
+                        yield return new CodeInstruction(OpCodes.Brtrue, returnTrue);
                     }
+                    yield return instrList[i];
                 }
-                foreach (CodeInstruction ci in instrList) yield return ci;
+                // append a "return true" at the end with my label
+                CodeInstruction loadTrue = new CodeInstruction(OpCodes.Ldc_I4_1);
+                loadTrue.labels.Add(returnTrue);
+                yield return loadTrue;
+                yield return new CodeInstruction(OpCodes.Ret);
             }
         }
     }
