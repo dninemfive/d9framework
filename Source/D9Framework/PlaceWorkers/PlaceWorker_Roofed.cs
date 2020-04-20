159f30d73f8b88042821cc091eea18ac04bd12e5
dninemfive <11084010+dninemfive@users.noreply.github.com>
2020-04-20 14:16:59 -0700

    make PlaceWorker_Roofed more consistent with vanilla, make PW_RH actually inherit it

--- Source/D9Framework/PlaceWorkers/PlaceWorker_Roofed.cs
+++ Source/D9Framework/PlaceWorkers/PlaceWorker_Roofed.cs
@@ -14,12 +14,9 @@ namespace D9Framework
     {
         public override AcceptanceReport AllowsPlacing(BuildableDef checkingDef, IntVec3 loc, Rot4 rot, Map map, Thing thingToIgnore = null, Thing thing = null)
         {
-            foreach (IntVec3 current in GenAdj.CellsOccupiedBy(loc, rot, checkingDef.Size))
+            if (!map.roofGrid.Roofed(loc))
             {
-                if (!map.roofGrid.Roofed(current))
-                {
-                    return new AcceptanceReport("D9F_Roofed_NeedsRoof".Translate(checkingDef.label));
-                }
+                return new AcceptanceReport("D9F_Roofed_NeedsRoof".Translate(checkingDef.label));
             }
             return true;
         }
@@ -28,11 +25,11 @@ namespace D9Framework
     /// Require that the building be placed "hanging" from a roof, i.e. that the tile is roofed, it's not above anything which touches the roof, and it's not on the same tile as another RoofHanger
     /// </summary>
     /// <para>Originally by CuproPanda, for Additional Joy Objects.</para>
-    public class PlaceWorker_RoofHanger : PlaceWorker
+    public class PlaceWorker_RoofHanger : PlaceWorker_Roofed
     {
 		public override AcceptanceReport AllowsPlacing(BuildableDef checkingDef, IntVec3 loc, Rot4 rot, Map map, Thing thingToIgnore = null, Thing thing = null) {
             AcceptanceReport roofedReport = base.AllowsPlacing(checkingDef, loc, rot, map, thingToIgnore); //check if tile is roofed
-            if (!roofedReport.Accepted) return roofedReport;            
+            if (!roofedReport.Accepted) return roofedReport;
             foreach (IntVec3 c in GenAdj.CellsOccupiedBy(loc, rot, checkingDef.Size)) // Don't allow placing on big things
             {
                 if (c.GetEdifice(map) != null)
