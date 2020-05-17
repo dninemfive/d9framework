e4aeb43ba32968ab999f22182c92762a2b9fd218
dninemfive <11084010+dninemfive@users.noreply.github.com>
2020-05-16 22:02:04 -0700

    additional work on PatchOperationAddSafe

--- Source/D9Framework/PatchOperations/PatchOperationAddSafe.cs
+++ Source/D9Framework/PatchOperations/PatchOperationAddSafe.cs
@@ -12,7 +12,7 @@ namespace D9Framework
     /// <summary>
     /// Works exactly like PatchOperationAdd, except that if the parent node doesn't exist this operation creates it.
     /// </summary>
-    class PatchOperationAddSafe : PatchOperationAdd
+    class PatchOperationAddSafe : PatchOperationPathed
     {
         private enum Order { Append, Prepend }
         private XmlContainer value;
@@ -20,13 +20,64 @@ namespace D9Framework
 
         protected override bool ApplyWorker(XmlDocument xml)
         {
+            bool result = false;
+            XmlNode node = value.node;
             // select nodes with xpath
-            // for each parent:
-            //      if identical node exists,
-            //          append children to node
-            //      else,
-            //          create parent
-            //          append children to node
+            foreach(object item in xml.SelectNodes(base.xpath))
+            {
+                result = true;
+                XmlNode cur = item as XmlNode;
+                // for each parent:
+                if (parentOrder == Order.Append)
+                {
+                    foreach (XmlNode parent in node.ChildNodes) ConditionallyApplyParent(cur, parent);
+                }
+                else if(parentOrder == Order.Prepend)
+                {
+                    for (int i = cur.ChildNodes.Count - 1; i >= 0; i--) ConditionallyApplyParent(cur, node.ChildNodes[i]);
+                }                
+            }
+            return result;
+        }
+
+        private void ConditionallyApplyParent(XmlNode target, XmlNode parent)
+        {
+            bool identicalNodeExists = false;
+            foreach(XmlNode targetChild in target.ChildNodes)
+            {
+                // check for identical node
+                if (XmlNodesEqual(targetChild, parent))
+                {
+                    identicalNodeExists = true;
+                    // append children to existing node
+                }        
+            }
+            if (!identicalNodeExists)
+            {
+                // create node
+                // append children to parent
+                foreach (XmlNode child in parent.ChildNodes) AppendOrPrependNode(target, child, childOrder);
+            }
+        }
+
+        private static bool XmlNodesEqual(XmlNode a, XmlNode b)
+        {
+            return true;
+        }
+
+        private void AppendOrPrependNode(XmlNode target, XmlNode node, Order order)
+        {
+            if(order == Order.Append)
+            {
+                foreach (XmlNode childNode in node.ChildNodes) target.AppendChild(target.OwnerDocument.ImportNode(childNode, true));
+            }
+            else if(order == Order.Prepend)
+            {
+                for (int num = node.ChildNodes.Count - 1; num >= 0; num--)
+                {
+                    target.PrependChild(target.OwnerDocument.ImportNode(node.ChildNodes[num], true));
+                }
+            }
         }
     }
 }
