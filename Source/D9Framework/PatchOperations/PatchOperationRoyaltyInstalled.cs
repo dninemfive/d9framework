1fb0a033022ecc8576e56b410af5bd824d8d959c
dninemfive <11084010+dninemfive@users.noreply.github.com>
2020-06-06 13:04:20 -0700

    add

---
+++ Source/D9Framework/PatchOperations/PatchOperationRoyaltyInstalled.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+using RimWorld;
+using System.Xml;
+
+namespace D9Framework
+{
+    class PatchOperationRoyaltyInstalled : PatchOperation
+    {
+# pragma warning disable CS0649
+        private PatchOperation match, nomatch;
+#pragma warning restore CS0649
+
+        protected override bool ApplyWorker(XmlDocument xml)
+        {
+            if (ModLister.RoyaltyInstalled)
+            {
+                if (match != null) return match.Apply(xml);
+            }                
+            else
+            {
+                if (nomatch != null) return nomatch.Apply(xml);
+            }                
+            return true;
+        }
+    }
+}
