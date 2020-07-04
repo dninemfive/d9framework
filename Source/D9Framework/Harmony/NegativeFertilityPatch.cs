8505ddf317d25d12ce774bddb1e858355e772f09
dninemfive <11084010+dninemfive@users.noreply.github.com>
2020-07-04 13:28:45 -0700

    improve Negative Fertility Patch

--- Source/D9Framework/Harmony/NegativeFertilityPatch.cs
+++ Source/D9Framework/Harmony/NegativeFertilityPatch.cs
@@ -10,20 +10,45 @@ using RimWorld;
 
 namespace D9Framework
 {
+    [StaticConstructorOnStartup]
     static class NegativeFertilityPatch
     {
+        public static float EffectiveMaxFertility;
+
+        static NegativeFertilityPatch()
+        {
+            IEnumerable<TerrainDef> terrainsByFertility = (from td in DefDatabase<TerrainDef>.AllDefsListForReading
+                                                           orderby td.fertility descending
+                                                           select td);
+            if (terrainsByFertility.EnumerableNullOrEmpty())
+            {
+                ULog.Error("Negative Fertility Patch: terrainsByFertility was empty. Setting EffectiveMaxFertility to 1.");
+                EffectiveMaxFertility = 1f;
+            }
+            else
+            {
+                EffectiveMaxFertility = terrainsByFertility.ElementAt(0).fertility;
+            }
+        }
+
         [HarmonyPatch(typeof(Plant), nameof(Plant.GrowthRateFactor_Fertility), MethodType.Getter)]
         class NegativeFertilityPostfix
         {
             [HarmonyPostfix]
             public static void GrowthRateFactor_FertilityPostfix(ref float __result, ref Plant __instance)
             {
-                if (__instance.def.HasModExtension<UseNegativeFertility>())
+                UseNegativeFertility me;
+                if ((me = __instance.def.GetModExtension<UseNegativeFertility>()) != null)
                 {
-                    __result = Mathf.Clamp01(1f - __instance.Map.fertilityGrid.FertilityAt(__instance.Position)) * __instance.def.plant.fertilitySensitivity + (1f - __instance.def.plant.fertilitySensitivity);
+                    __result = Mathf.Clamp((EffectiveMaxFertility - __instance.Map.fertilityGrid.FertilityAt(__instance.Position)) * __instance.def.plant.fertilitySensitivity + (1f - __instance.def.plant.fertilitySensitivity),
+                               me.minFertility, 
+                               me.maxFertility);
                 }
             }
         }
     }
-    public class UseNegativeFertility : DefModExtension { }
+    public class UseNegativeFertility : DefModExtension
+    {
+        public float minFertility = 0.05f, maxFertility = 1f;
+    }
 }
