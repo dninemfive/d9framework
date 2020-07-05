15b6f464441f26b56413ae1ed6bf2c4ba62efc31
dninemfive <11084010+dninemfive@users.noreply.github.com>
2020-07-05 12:57:17 -0700

    initial work on double-stuffed things, gonna need custom recipes to make

---
+++ Source/D9Framework/Misc/DoubleStuffThing.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Verse;
+using RimWorld;
+
+namespace D9Framework.Misc
+{
+    class DoubleStuffThing : ThingWithComps
+    {
+        public ThingDef StuffTwo => stuffTwo;
+        private ThingDef stuffTwo;
+
+        public override Color DrawColorTwo
+        {
+            get
+            {
+                if (StuffTwo != null) return def.GetColorForStuff(StuffTwo);
+                return def.graphicData?.color ?? Color.white;
+            }
+        }
+        public void SetStuffTwoDirect(ThingDef newStuffTwo)
+        {
+            stuffTwo = newStuffTwo;
+        }
+        public override void ExposeData()
+        {
+            base.ExposeData();
+            Scribe_Defs.Look(ref stuffTwo, "stuffTwo");
+        }
+    }
+    public class SecondStuff : DefModExtension
+    {
+# pragma warning disable CS0649
+        public int stuffTwoCost;
+        public List<StuffCategoryDef> stuffTwoCategories;
+# pragma warning restore CS0649
+    }
+}
