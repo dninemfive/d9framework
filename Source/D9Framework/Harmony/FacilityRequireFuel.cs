4b20b6c945f18283ed8519f41dd5f3920e7b5192
dninemfive <11084010+dninemfive@users.noreply.github.com>
2020-07-07 10:03:22 -0700

    add FacilityRequireFuel patch

---
+++ Source/D9Framework/Harmony/FacilityRequireFuel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using HarmonyLib;
+using Verse;
+using RimWorld;
+
+namespace D9Framework
+{
+    static class FacilityRequireFuel
+    {
+        [HarmonyPatch(typeof(CompFacility), nameof(CompFacility.CanBeActive), MethodType.Getter)]
+        class FacilityRequireFuelPatch
+        {
+            [HarmonyPostfix]
+            public static void CanBeActivePostfix(ref bool __result, ref CompFacility __instance)
+            {
+                CompRefuelable fuel = __instance.parent.TryGetComp<CompRefuelable>();
+                __result &= (fuel == null || fuel.HasFuel);
+            }
+        }
+    }
+}
