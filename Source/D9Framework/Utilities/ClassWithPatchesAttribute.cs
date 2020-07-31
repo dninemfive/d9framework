2d4468bc179ca6c8786ae7dcf318747256c3006e
dninemfive <11084010+dninemfive@users.noreply.github.com>
2020-07-31 14:19:29 -0700

    initial work on annotation system

---
+++ Source/D9Framework/Utilities/ClassWithPatchesAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D9Framework
+{
+    /// <summary>
+    /// Used to make adding new Harmony patches internally easier. Not intended for other mods' use, but can likely be used.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class)]
+    class ClassWithPatchesAttribute : Attribute
+    {
+        public string PlainName, Key, DescKey;
+
+        public ClassWithPatchesAttribute(string pn, string k, string dk)
+        {
+            PlainName = pn;
+            Key = k;
+            DescKey = dk;
+        }
+    }
+}
--- Source/D9Framework/Utilities/D9FrameworkDefOf.cs
+++ Source/D9Framework/Utilities/D9FrameworkDefOf.cs
@@ -8,12 +8,17 @@ using RimWorld;
 
 namespace D9Framework
 {
+    /// <summary>
+    /// Standard RimWorld <c>DefOf</c> class for use in various location within the mod. Not intended for external use, but can be used by others.
+    /// </summary>
     [DefOf]
     public static class D9FrameworkDefOf
     {
 #pragma warning disable CS0649
+        #region StatDefs
         public static StatDef HealingRateFactor;
         public static StatDef BleedRateFactor;
+        #endregion StatDefs
 #pragma warning restore CS0649
 
         static D9FrameworkDefOf()
